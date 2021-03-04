e0bfc8ead4629082020de5d185477dd41aa4e73d
Sergio <sergiolozada124@gmail.com>
2021-03-04 14:04:10 -0500

    delet icon path

--- src/index.js
+++ src/index.js
@@ -7,12 +7,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import { icons } from './assets/icons'
-
 import { Provider } from 'react-redux'
 import store from './store'
-
-React.icons = icons
 
 ReactDOM.render(
   <Provider store={store}>
